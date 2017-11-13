0e855c2697902b1925e560d9cb4dae792f4d8455
Ethan Fung <EthanEFung@gmail.com>
2017-11-13 00:32:26 -0800

    feat: finish first fortune 500 prompt question

---
+++ fortune_500_company_1/outSmart.js
@@ -0,0 +1,61 @@
+/**
+ * The first prompt that was handed to me in hind-sight was a very simple variation of the
+ * popular interview question, FIZZBUZZ. Regretfully, I ran out of time, but I had a very 
+ * clear idea of what I wanted to do that if given just a minute more, I would have solved
+ * the problem.
+ * 
+ * PROMPT:
+ * given an input of a positive integer n, and two integers p and q render a sequence
+ * of integers seperated by commas decrementing from n til 1, with the following constraints:
+ * (1) if integer in the sequence is divisible by p or q print in sequence the word "OUT"
+ * instead of the integer
+ * (2) if the decimal representation of the number in sequence contained either p or q,
+ * render the word "SMART" instead of the integer
+ * (3) if conditions 1 AND 2 are met, render OUTTHINK instead of the integer
+ */
+
+function renderOutSmartSequence(n, p, q) {
+  let integer = processInt(n, p, q);
+  if (integer === "INVALID") return "INVALID INTEGER RECEIVED";
+  if (n === 1) return integer;
+  return integer + ", " + renderOutSmartSequence(n - 1, p, q);
+}
+
+function processInt(n, p, q) {
+  if (!areValidIntegers(n, p, q)) return "INVALID";
+  let output = "";
+  const int = new Integer(n);
+
+  if (int.isDivisibleBy(p) || int.isDivisibleBy(q)) {
+    output += "OUT";
+  }
+  if (int.decimalContains(p) || int.decimalContains(q)) {
+    output += "SMART";
+  }
+  if (output.length === 0) return n.toString();
+  return output;
+}
+
+function areValidIntegers(...ints) {
+  return ints.every(int => typeof int === "number");
+}
+
+function Integer(x) {
+  this.isDivisibleBy = function(y) {
+    return x % y === 0;
+  };
+  this.decimalContains = function(y) {
+    let stringInt = x.toString();
+    for (let i = 0; i < stringInt.length; i++) {
+      if (stringInt[i] === y.toString()) return true;
+    }
+    return false;
+  };
+}
+
+module.exports = {
+  renderOutSmartSequence,
+  areValidIntegers,
+  processInt,
+  Integer
+};
---
+++ fortune_500_company_1/outSmart.test.js
@@ -0,0 +1,74 @@
+const {
+  renderOutSmartSequence,
+  areValidIntegers,
+  processInt,
+  Integer
+} = require("./outSmart.js");
+
+describe("outSmart sequence function", function() {
+  it("should accept an integer as its argument", function() {
+    let actual = renderOutSmartSequence(10, 4, 3);
+    let expected = "10, OUT, OUT, 7, OUT, 5, OUTSMART, OUTSMART, 2, 1";
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should break if anything other than integers passed in as arguments", function() {
+    let actual = renderOutSmartSequence(13, "hi", 3);
+    let expected = "INVALID INTEGER RECEIVED";
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("areValidIntegers function", function() {
+  it("should validate the type of data is a number", function() {
+    let actual = areValidIntegers(1, 2, 3, 4, 5);
+    expect(actual).toBeTruthy();
+  });
+
+  it("should validate the type of data is not a number", function() {
+    let actual = areValidIntegers(1, 2, {}, 4, 5);
+    expect(actual).toBeFalsy();
+  });
+});
+
+describe("processInt function", function() {
+  it("should render OUT if n is divisible by p or q", function() {
+    let actual = processInt(15, 3, 7);
+    let expected = "OUT";
+    expect(actual).toBe(expected);
+  });
+
+  it("should render SMART if decimal representation contains p or q", function() {
+    let actual = processInt(17, 3, 7);
+    let expected = "SMART";
+    expect(actual).toBe(expected);
+  });
+
+  it("should render OUTSMART if conditions 1 and 2 are both met", function() {
+    let actual = processInt(17, 1, 7);
+    let expected = "OUTSMART";
+    expect(actual).toBe(expected);
+  });
+});
+
+describe("Integer constructor", function() {
+  it("should check if int is divisible by another int", function() {
+    const three = new Integer(3);
+    let actual = three.isDivisibleBy(1);
+    expect(actual).toBeTruthy();
+  });
+
+  it("should check if int decimal representation contains another int", function() {
+    const ten = new Integer(10);
+    let actual = ten.decimalContains(0);
+    expect(actual).toBeTruthy();
+  });
+
+  it("should break if a number isnt passed in to integer constructor", function() {
+    const nonInt = new Integer();
+    let actual = nonInt.isDivisibleBy(1);
+    expect(actual).toBeFalsy();
+  });
+});
